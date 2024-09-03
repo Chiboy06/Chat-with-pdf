573cceee685fa538d899b9bf5c9f71947e0c13c3
Chinaza <elechukwujosh3@gmail.com>
2024-09-03 07:26:01 +0200

    implement text area, pdf view for mobile, mobile resposive

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,7 @@ export const dynamic = "force-dynamic"
 
 function Dashboard() {
   return (
-    <div className='h-full max-w-7xl  mx-auto'>
+    <div className='h-full max-w-7xl w-screen'>
         <h1 className='text-3xl p-5 bg-transparent font-extralight text-gray-200'>
             My Documents
           </h1>
